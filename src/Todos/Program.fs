d8cffbcb60b32bcc325c6ac585341b8f13dcd139
Ryan McDougall <rp.mcdougall@gmail.com>
2019-05-04 18:35:12 -0500

    switched to multi solution project

---
+++ src/Todos/Program.fs
@@ -0,0 +1,38 @@
+﻿// Learn more about F# at http://fsharp.org
+open FsharpTodos
+
+open Todos
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+open MongoDB.Driver
+open TodosLib.Http
+open TodoMongoDB
+
+let routes =
+    choose [
+        TodoHttp.handlers
+    ]
+
+let configureApp (app: IApplicationBuilder) =
+    app.UseGiraffe routes
+    
+
+let configureServices (services: IServiceCollection) =
+    let mongo = MongoClient(Environment.GetEnvironmentVariable "MONGO_URL")
+    let db = mongo.GetDatabase "todos"
+    services.AddGiraffe() |> ignore
+    services.AddTodoMongoDB(db.GetCollection<Todo>("todos")) |> ignore
+    
+
+[<EntryPoint>]
+let main _ =
+    WebHostBuilder()
+        .UseKestrel()
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(configureServices)
+        .Build()
+        .Run()
+    0 // return an integer exit code
